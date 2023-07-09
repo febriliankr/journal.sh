a6240cf97319387844a01ba2b50ca1c1df74f4e4
Febrilian <febrilian.kr@gmail.com>
2023-07-09 10:38:51 +0700

    Added appending date to the top of the line

--- journal.sh
+++ journal.sh
@@ -1,18 +1,21 @@
+# +r\!jdate appends date to the top 
 printf "📕 Now/tomorrow/yesterday/idea/todo (n/t/y/i/d)"
 read DAY
 if [ "$DAY" = "t" ]; then
   tomorrow=`date -v+1d +%Y_%m_%d`
-  nvim ~/Library/Mobile\ Documents/iCloud\~com\~logseq\~logseq/Documents/notes/journals/$tomorrow.md
+  nvim ~/Documents/notes/journals/$tomorrow.md
 elif [ "$DAY" = "n" ]; then
   now=`date +%Y_%m_%d`
-  nvim ~/Library/Mobile\ Documents/iCloud\~com\~logseq\~logseq/Documents/notes/journals/$now.md
+  nvim ~/Documents/notes/journals/$now.md
 elif [ "$DAY" = "y" ]; then
   yesterday=`date -v-1d +%Y_%m_%d`
-  nvim ~/Library/Mobile\ Documents/iCloud\~com\~logseq\~logseq/Documents/notes/journals/$yesterday.md
+  nvim ~/Documents/notes/journals/$yesterday.md
 elif [ "$DAY" = "i" ]; then
-  nvim ~/Library/Mobile\ Documents/iCloud\~com\~logseq\~logseq/Documents/notes/pages/Fleeting\ Notes\ .\ Startup.md
+  nvim ~/Documents/notes/pages/Fleeting\ Notes\ .\ Startup.md +r\!jdate
 elif [ "$DAY" = "d" ]; then
-  nvim ~/Library/Mobile\ Documents/iCloud\~com\~logseq\~logseq/Documents/notes/pages/Todo.md
+  nvim ~/Documents/notes/pages/Todo.md
+elif [ "$DAY" = "r" ]; then
+  nvim ~/Documents/notes/pages/Risetku.md +r\!jdate
 else
     echo "invalid input"
     exit 1
